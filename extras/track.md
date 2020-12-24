cf066dad1dfe131433baee42ce438e454dac6897
Prashant <prshntsuyl@gmail.com>
2020-12-24 15:08:14 +0100

    add

---
+++ extras/track.md
@@ -0,0 +1,17 @@
+| Day | Todo | Completed
+--- | --- | ---
+24 Dec | 6 | 6
+25 Dec | 6 | 12
+26 Dec | 6 | 18
+27 Dec | 6 | 24
+28 Dec | 3 | 27
+29 Dec | 3 | 30
+30 Dec | 6 | 36
+31 Dec | 6 | 42
+1 Jan | 6 | 48
+2 Jan | 6 | 54
+3 Jan | 6 | 60
+4 Jan | 3 | 63
+5 Jan | 3 | 66
+6 Jan | 3 | 69
+7 Jan | 3 | 72
--- practice/array/Anagram.java
+++ practice/array/Anagram.java
@@ -2,25 +2,25 @@ package practice.array;
 
 class Anagram {
   public static void main(String[] args) {
-          System.out.println(isAnagram("cat", "tac"));
+    System.out.println(isAnagram("cat", "tac"));
   }
 
   static boolean isAnagram(String a, String b) {
 
-          if(a.length() != b.length()) {
-                  return false;
-          }
+    if (a.length() != b.length()) {
+      return false;
+    }
 
-          for (int i=0; i < b.length(); i++) {
-                  String ch = b.charAt(i) + "";
-                  if(!a.contains(ch)) {
-                          return false;
-                  }
+    for (int i = 0; i < b.length(); i++) {
+      String ch = b.charAt(i) + "";
+      if (!a.contains(ch)) {
+        return false;
+      }
 
-                  a.replaceFirst(ch, "");
+      a.replaceFirst(ch, "");
 
-          }
+    }
 
-          return true;
+    return true;
   }
 }
