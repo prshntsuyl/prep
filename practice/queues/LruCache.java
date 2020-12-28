70ed872120247707deb94dfbf69aacb9e6a9ad6c
Prashant <prshntsuyl@gmail.com>
2020-12-28 23:00:27 +0100

    lru cache

---
+++ practice/queues/LruCache.java
@@ -0,0 +1,29 @@
+package practice.queues;
+
+import java.util.Queue;
+
+class LruCache {
+  public static void main(String[] args) {
+    System.out.println("!!!");
+  }
+
+  // use doubly linked list and HashMap to solve this
+  static class LRUCache {
+    Queue<Integer> q1 = new Queue<>();
+
+    LRUCache(int cap) {
+      // Intialize the cache capacity with the given
+      // cap
+    }
+
+    // This method works in O(1)
+    public static int get(int key) {
+      // your code here
+    }
+
+    // This method works in O(1)
+    public static void set(int key, int value) {
+      // your code here
+    }
+  }
+}
