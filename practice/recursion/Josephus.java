644d19883cdb2aa585b91ff6fa0b59c8446be77a
Prashant <prshntsuyl@gmail.com>
2020-12-30 15:34:33 +0100

    add test

--- practice/recursion/Josephus.java
+++ practice/recursion/Josephus.java
@@ -4,6 +4,7 @@ class Josephus {
 
     public static void main(String[] args) {
         System.out.println(josephus(7, 3));
+        System.out.println("testting");
     }
 
     static int josephus(int n, int k) {
