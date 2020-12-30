346b7bdff232bb1caf1483df61cd01b09ecc0af7
Prashant <prshntsuyl@gmail.com>
2020-12-31 00:19:34 +0100

    ignore

--- practice/heapify/HeapSort.java
+++ practice/heapify/HeapSort.java
@@ -1,5 +1,7 @@
 package practice.heapify;
 
+import java.util.Map;
+
 /**
  * HeapSort
  */
@@ -13,17 +15,16 @@ public class HeapSort {
     public static void main(String[] args) {
         int N = 5;
         int[] arr = { 4, 1, 3, 9, 7 };
-
-        System.out.println(heapify(arr, N));
+        heapify(arr, N, N);
     }
 
-    void buildHeap(int arr[], int n) {
+    void buildHeap(int[] arr, int n) {
         // Your code here
     }
 
     // To heapify a subtree rooted with node i which is
     // an index in arr[]. n is size of heap
-    void heapify(int arr[], int n, int i) {
+    static void heapify(int[] arr, int n, int i) {
         // Your code here
     }
 }
