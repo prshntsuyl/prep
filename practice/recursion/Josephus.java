1a3152ef6bf6ddf6c849a75b9ef015ae862a87c5
Prashant <prshntsuyl@gmail.com>
2020-12-30 15:15:18 +0100

    testing commit from fugitive

--- practice/recursion/Josephus.java
+++ practice/recursion/Josephus.java
@@ -2,15 +2,15 @@ package practice.recursion;
 
 class Josephus {
 
-  public static void main(String[] args) {
-    System.out.println(josephus(7, 3));
-  }
-
-  static int josephus(int n, int k) {
-    // Your code here
-    if (n == 1) {
-      return 1; // josephus(1, k) = 1
+    public static void main(String[] args) {
+        System.out.println(josephus(7, 3));
     }
-    return ((josephus(n - 1, k)) + k - 1) % n + 1;
-  }
+
+    static int josephus(int n, int k) {
+        // Your code here
+        if (n == 1) {
+            return 1; // josephus(1, k) = 1
+        }
+        return ((josephus(n - 1, k)) + k - 1) % n + 1;
+    }
 }
