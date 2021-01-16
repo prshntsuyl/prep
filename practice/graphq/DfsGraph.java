b3fe9c797430130ce71ee752da09ea626f81c0a3
prshntsuyl <prshntsuyl@gmail.com>
2021-01-16 01:13:00 +0100

    dfs of graphs solution

---
+++ practice/graphq/DfsGraph.java
@@ -0,0 +1,107 @@
+package practice.graphq;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * DfsGraph
+ */
+public class DfsGraph {
+
+    /**
+     * Given a connected undirected graph. Perform a Depth First Traversal of the
+     * graph. Note: Use recursive approach to find the BFS traversal of the graph
+     * starting from the 0th vertex from left to right according to the graph..
+     **/
+    public static void main(String[] args) {
+        System.out.println("Hello There!!");
+        Graph graph = new Graph();
+
+        graph.addVertex(0);
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(4, 5);
+
+        graph.dfs(0);
+
+        System.out.println("Lets Try ANother!!!");
+
+        Graph someGraph = new Graph();
+
+        someGraph.addVertex(0);
+        someGraph.addVertex(1);
+        someGraph.addVertex(2);
+        someGraph.addVertex(3);
+        someGraph.addVertex(4);
+        someGraph.addVertex(5);
+        someGraph.addVertex(6);
+        someGraph.addVertex(7);
+
+        someGraph.addEdge(0, 1);
+        someGraph.addEdge(0, 2);
+        someGraph.addEdge(0, 3);
+        someGraph.addEdge(3, 5);
+        someGraph.addEdge(3, 4);
+        someGraph.addEdge(5, 6);
+        someGraph.addEdge(5, 7);
+
+        someGraph.dfs(0);
+
+        System.out.println("Lets Try ANother!!!");
+        System.out.println();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+
+        ArrayList<Integer> zeroAdj = new ArrayList<Integer>(Arrays.asList(1, 2));
+        ArrayList<Integer> firstAdj = new ArrayList<Integer>(Arrays.asList(3, 4));
+        ArrayList<Integer> secondAdj = new ArrayList<Integer>(Arrays.asList());
+        ArrayList<Integer> thirdAdj = new ArrayList<Integer>(Arrays.asList());
+        ArrayList<Integer> fourthAdj = new ArrayList<Integer>(Arrays.asList(5));
+        ArrayList<Integer> fifthAdj = new ArrayList<Integer>(Arrays.asList());
+
+        adj.add(zeroAdj);
+        adj.add(firstAdj);
+        adj.add(secondAdj);
+        adj.add(thirdAdj);
+        adj.add(fourthAdj);
+        adj.add(fifthAdj);
+
+        ArrayList<Integer> result = dfsOfGraph(0, adj);
+
+        for (Integer integer : result) {
+            System.out.print(integer + " ");
+        }
+    }
+
+    static ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        boolean[] isVisited = new boolean[adj.size()];
+        dfsRecursive(0, adj, isVisited, result);
+
+        return result;
+    }
+
+    static void dfsRecursive(int current, ArrayList<ArrayList<Integer>> adj, boolean[] isVisited,
+            ArrayList<Integer> result) {
+        isVisited[current] = true;
+        visit(current, result);
+        for (int dest : adj.get(current)) {
+            if (!isVisited[dest])
+                dfsRecursive(dest, adj, isVisited, result);
+        }
+    }
+
+    static void visit(int value, ArrayList<Integer> result) {
+        result.add(value);
+    }
+
+}
