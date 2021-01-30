19184b2b3860363a455307858703c74c662368d7
Prashant <prshntsuyl@gmail.com>
2021-01-30 17:32:22 +0100

    knapsack brute, bottoms up and top down

--- practice/dynamic/Knapsack.java
+++ practice/dynamic/Knapsack.java
@@ -12,33 +12,33 @@ public class Knapsack {
      */
     public static void main(String[] args) {
         //answer should be 6
-        System.out.println(knapSack(4, new int[]{1, 2, 3, 4, 5}, new int[]{1, 2, 5, 4, 3}, 5));
-        System.out.println(knapSackO(4, new int[]{1, 2, 3, 4, 5}, new int[]{1, 2, 5, 4, 3}, 5));
+        System.out.println(knapSackTB(4, new int[]{1, 2, 3, 4, 5}, new int[]{1, 2, 5, 4, 3}, 5));
+        System.out.println(knapSackMemoized(4, new int[]{1, 2, 3, 4, 5}, new int[]{1, 2, 5, 4, 3}, 5));
         System.out.println(knapSackBU(4, new int[]{1, 2, 3, 4, 5}, new int[]{1, 2, 5, 4, 3}, 5));
 
     }
 
     // das ist brute force
-    static int knapSack(int W, int wt[], int val[], int n) {
+    static int knapSackTB(int W, int wt[], int val[], int n) {
         // your code here
-        return knapSackLow(W, wt, val, 0);
+        return knapSackTopToBottom(W, wt, val, 0);
     }
 
-    static int knapSackLow(int W, int[] wt, int[] val, int i) {
+    static int knapSackTopToBottom(int W, int[] wt, int[] val, int i) {
         // your code here
         if (i == wt.length) {
             return 0;
         }
 
         if (W - wt[i] < 0) {
-            return knapSackLow(W, wt, val, i + 1);
+            return knapSackTopToBottom(W, wt, val, i + 1);
         }
 
-        return Math.max(knapSackLow(W - wt[i], wt, val, i + 1) + val[i], knapSackLow(W, wt, val, i + 1));
+        return Math.max(knapSackTopToBottom(W - wt[i], wt, val, i + 1) + val[i], knapSackTopToBottom(W, wt, val, i + 1));
     }
 
     // optimum
-    static int knapSackO(int W, int wt[], int val[], int n) {
+    static int knapSackMemoized(int W, int wt[], int val[], int n) {
         // your code here
         Map<Integer, Map<Integer, Integer>> cache = new HashMap<Integer, Map<Integer, Integer>>();
 
@@ -66,18 +66,20 @@ public class Knapsack {
     }
 
     static int knapSackBU(int W, int wt[], int val[], int n) {
+//        this is a matrix 2d array
         int[][] cache = new int[wt.length + 1][W + 1];
 
         for (int i = 1; i <= wt.length; i++) {
             for (int j = 0; j <= W; j++) {
-                if (wt[i - 1] > j) {
+                if (wt[i - 1] > j) { //when weight is greater than the capacity
                     cache[i][j] = cache[i - 1][j];
                 } else {
-                    cache[i][j] = Math.max(cache[i - 1][j], cache[i - 1][j - wt[i - 1]] + val[i - 1]);
+                    cache[i][j] = Math.max(cache[i - 1][j - wt[i - 1]] + val[i - 1], cache[i - 1][j]);
                 }
             }
         }
 
+        //just returns the last element of the matrix
         return cache[wt.length][W];
     }
 }
