9e0b6ead4cd57174bbd2271fdae9747c051533a6
Prashant <prshntsuyl@gmail.com>
2021-02-02 03:21:14 +0100

    Nmeetings working

--- practice/dynamic/greedy/Nmeetings.java
+++ practice/dynamic/greedy/Nmeetings.java
@@ -10,24 +10,19 @@ class Nmeetings {
     //constrain:: start time of one chosen meeting can't be equal to the end time of the other chosen meeting.
     public static void main(String[] args) {
         int N = 6;
-        int[] S = new int[] {1, 3, 0, 5, 8, 5};
-        int[] F = new int[] {2, 4, 6, 7, 9, 9};
+        int[] S = new int[] {1, 3, 5, 0, 5, 8};
+        int[] F = new int[] {2, 4, 9, 6, 7, 9};
 
         System.out.println(maxMeetings(S, F, N));
-        // System.out.println(maxMeetings(S, F, N));
 
-        int N1 = 8;
-        int[] S1 = new int[] {75250, 50074, 43659, 8931, 11273,
-                              27545, 50879, 77924
-                             };
-        int[] F1 = new int[] {112960, 114515, 81825, 93424, 54316,
-                              35533, 73383, 160252
-                             };
+        int N1 = 30;
+        int[] S1 = new int[] {48, 43, 61, 54, 99, 84, 3, 3, 59, 30, 45, 72, 24, 87, 21, 48, 54, 88, 8, 67, 41, 64, 87, 54, 5, 62, 87, 33, 74, 92};
+        int[] F1 = new int[] {150, 67, 137, 131, 139, 115, 49, 6, 117, 126, 59, 109, 27, 96, 73, 60, 99, 108, 50, 145, 68, 104, 102, 82, 7, 126, 118, 93, 148, 150};
 
-        // System.out.println(maxMeetings(S1, F1, N1));
+        System.out.println(maxMeetings(S1, F1, N1));
     }
 
-    public static class Node extends Comparable<Node>{
+    public static class Node implements Comparable<Node> {
 
         public int startInd;
         public int endInd;
@@ -41,46 +36,44 @@ class Nmeetings {
             return this.endInd - n.endInd;
         }
 
-    } 
+    }
 
     public static int maxMeetings(int[] start, int end[], int n) {
-
-        List<Node> nodeList = new ArryaList<Node>();
-        for (int i=0; i<start.length; i++) {
-           nodeList.add(new Node(start[i], end[j]));
+        List<Node> nodeList = new ArrayList<Node>();
+        for (int i = 0; i < start.length; i++) {
+            nodeList.add(new Node(start[i], end[i]));
         }
 
         Collections.sort(nodeList);
 
+        int i = 0;
         for (Node node : nodeList) {
-             System.out.println(node.startInd + "," + node.endInd);
-         } 
+            start[i] = node.startInd;
+            end[i] = node.endInd;
+            i++;
 
-        return 0; 
-        // return maxMeetingsOnSorted(start, end, n);
+        }
+
+        return maxMeetingsOnSorted(start, end, n);
     }
 
     public static int maxMeetingsOnSorted(int[] start, int end[], int n) {
-        // add your code here(start);
-
         int count = 1;
         int k = 0;
-        for (int i = 0; i < start.length; i++) {
 
-			if(i + 1 == start.length) {
-			    i++;
-			    continue;
-			}
-
-            if (end[k] != start[i + 1] && end[k] < start[i + 1]) {
+        for (int i = 1; i < start.length; i++) {
+           if (end[k] < start[i]) {
                 count++;
+                //last selected item at i
                 k = i;
             }
         }
-        
+
         return count;
     }
 
+    //First attempt
+    //Input
     // N = 6
     // S[] = {1,3,0,5,8,5}
     // F[] = {2,4,6,7,9,9}
